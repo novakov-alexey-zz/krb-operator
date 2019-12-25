d6140953abd869c007ba4608368f147f98df86ec
Alexey Novakov <novakov.alex@gmail.com>
2019-12-25 11:16:15 +0100

    update script

--- run-docker.sh
+++ run-docker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 docker run \
   -e APP_CONFIG_PATH=/opt/docker/resources/application.conf \
-  -e KRB5_TEMPLATE_PATH=/opt/docker/resources/krb5-openshift-template.yaml \
   -v ~/.kube:/home/demiourgos728/.kube \
   -e K8S_SPECS_DIR=/opt/docker/resources \
-  krb-operator:0.1
+  alexeyn/kerberos-operator:0.1
