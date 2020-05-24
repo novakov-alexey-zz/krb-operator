39d3db0abc0b2d2656575e70e3504aae3261b292
Alexey Novakov <novakov.alex@gmail.com>
2020-05-24 17:16:48 +0200

    Update run-graal-docker.sh

--- run-graal-docker.sh
+++ run-graal-docker.sh
@@ -2,7 +2,6 @@
 docker run \
   -e APP_CONFIG_PATH=/opt/docker/resources/application.conf \
   -e LOGBACK_CONFIG_FILE=/opt/conf/logback.xml \
-  -e KUBERNETES_MASTER=https://kss-dev-env-adv-dns-a3625708.hcp.northeurope.azmk8s.io \
   -v /home/an/.kube/config:/root/.kube/config \
   -v ~/dev/git/krb-operator/src/main/resources:/opt/docker/resources \
   -v ~/dev/git/krb-operator/src/main/resources/logback.xml:/opt/conf/logback.xml \
