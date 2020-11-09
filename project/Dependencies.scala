5deaf11bc8253b4e871e61fbf0b9f27f4423f03c
Scala Steward <me@scala-steward.org>
2020-11-09 06:44:53 +0100

    Update scalatest to 3.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies extends AutoPlugin {
       val jacksonScalaVersion              = "2.11.3"
       val betterMonadicVersion             = "0.3.1"
       val freyaVersion                     = "0.2.7"
-      val scalaTestVersion                 = "3.2.2"
+      val scalaTestVersion                 = "3.2.3"
       val scalaTestCheckVersion            = "3.1.0.0-RC2"
       val scalaCheckVersion                = "1.15.1"
     }
