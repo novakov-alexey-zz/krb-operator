82996ba6363d9c3deb96cf1fccc5fb9038e6c2e0
Scala Steward <me@scala-steward.org>
2020-11-10 19:35:17 +0100

    Update openshift-client, ... to 4.13.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies extends AutoPlugin {
 
       val pureConfigVersion                = "0.14.0"
       val scalaLoggingVersion              = "3.9.2"
-      val fabric8K8sVersion                = "4.12.0"
+      val fabric8K8sVersion                = "4.13.0"
       val codecsVersion                    = "1.15"
       val jacksonJsonSchemaV               = "1.0.39"
       val jacksonScalaVersion              = "2.11.3"
