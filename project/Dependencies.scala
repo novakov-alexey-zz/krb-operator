a67b1bc01a4c77edd5da6015376336ff2fe909aa
Scala Steward <me@scala-steward.org>
2020-11-29 14:42:02 +0100

    Update jackson-module-scala to 2.12.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -19,7 +19,7 @@ object Dependencies extends AutoPlugin {
       val fabric8K8sVersion                = "4.13.0"
       val codecsVersion                    = "1.15"
       val jacksonJsonSchemaV               = "1.0.39"
-      val jacksonScalaVersion              = "2.11.3"
+      val jacksonScalaVersion              = "2.12.0"
       val betterMonadicVersion             = "0.3.1"
       val freyaVersion                     = "0.2.10"
       val scalaTestVersion                 = "3.2.3"
