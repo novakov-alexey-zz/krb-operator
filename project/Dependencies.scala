4356f77c7d851b2adfa9ae26fde32cdd00c49050
Scala Steward <me@scala-steward.org>
2021-06-30 20:55:46 +0200

    Update openshift-client, ... to 5.5.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies extends AutoPlugin {
 
       val pureConfigVersion                = "0.16.0"
       val scalaLoggingVersion              = "3.9.4"
-      val fabric8K8sVersion                = "5.4.1"
+      val fabric8K8sVersion                = "5.5.0"
       val codecsVersion                    = "1.15"
       val jacksonJsonSchemaV               = "1.0.39"
       val jacksonScalaVersion              = "2.12.3"
